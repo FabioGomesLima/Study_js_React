d8c4a6d32fae84b7140bd33bb7c58c6361d70997
FabioGomesLima <abioarturgomesdelima13@gmail.com>
2024-08-09 15:42:47 -0300

    Componentes Basicos

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* Aula de ReactNative :D */}
+      <Text style={{ marginTop: 100, color: 'green' }}>Olá Mundo!</Text>
+      <Text style={estilo}>Primeira Aula</Text> 
+      <Text>{1 + 5}</Text>
+      <Text style={estilo_2.instituicao}>IFRN</Text>
+      <Text style={estilo_2.disciplina}>Teste de Software</Text>
+      <Image style={estilo_2.image} source={require('./img/images.jpeg')} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const estilo_2 = StyleSheet.create({
+  instituicao: {
+    textAlign: 'right',
+    color: 'gray',
   },
+  disciplina: {
+    fontStyle: 'italic',
+  },
+  image: {
+    width: 100,
+    height: 100,
+  }
 });
+
+const estilo = {
+  fontSize: 20,
+  color: 'red',
+};
+
+
+
+
+
+
